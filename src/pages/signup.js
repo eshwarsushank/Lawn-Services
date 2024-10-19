95d3a48f644f6277b7ea0625e20e4a53197ef231
eshwarsushank <eshwarsushank@gmail.com>
2024-10-18 21:37:07 -0500

    Modified login page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,3 +1,10 @@
 export default function SignUp(){
-    return <h1>Login/SignUp</h1>
+    return(<> <h1>This is login page </h1>
+    
+      <p> Username</p> <input type = "Username" ></input>
+      <p> password</p> <input type = "password" ></input></>
+
+        
+)
+
 }
