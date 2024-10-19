6db8fe62f5964008d645faba1261d3d91d7f63a9
eshwarsushank <eshwarsushank@gmail.com>
2024-10-18 21:51:25 -0500

    Added Signup button

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,9 +2,10 @@ export default function SignUp(){
     return(<> <h1>This is login page </h1>
     
       <p> Username</p> <input type = "Username" ></input>
-      <p> password</p> <input type = "password" ></input></>
+      <p> password</p> <input type = "password" ></input>
+      <center><p>Not a member? </p></center>SignUp
 
-        
+      </>
 )
 
 }
